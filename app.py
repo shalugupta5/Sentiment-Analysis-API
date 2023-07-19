83507dfac2f413beccf2780a74054d7713bd4c98
Shalu Gupta <112753764+shalugupta5@users.noreply.github.com>
2023-07-19 23:20:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,20 @@
-from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from textblob import TextBlob
-#import uvicorn
+from flask import Flask
 
-app = FastAPI(title="Sentiment Analysis API")
 
-@app.get("/", include_in_schema=False)
-def index():
-    return RedirectResponse("/docs", status_code=308)
 
-@app.get("/sentiment-analysis/{text}")
-def sentiment_analysis(text: str):
-    blob = TextBlob(text)
-    polarity = blob.sentiment.polarity
-    subjectivity = blob.sentiment.subjectivity
+app = Flask(__name__)
 
-    if polarity > 0:
-        sentiment = "positive"
-    elif polarity < 0:
-        sentiment = "negative"
-    else:
-        sentiment = "neutral"
 
-    return {
-        "text": text,
-        "sentiment": sentiment,
-        "polarity": polarity,
-        "subjectivity": subjectivity
-    }
 
-#if __name__ == '__main__':
-    #uvicorn.run('app:app', host='0.0.0.0', port=8000)
+# mysql = MySQL(app)
+
+
+
+
+
+@app.route('/')
+def hello():
+    return 'Hello, Flask app with a virtual environment!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
